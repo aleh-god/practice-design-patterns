cdfd8c6ca714aa8a0e51503a924aebc581edd3b8
aleh-god <aleh.god@gmail.com>
2022-10-01 10:45:46 +0300

    add: Build source module.

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,9 @@
+object ProjectConfig {
+    const val appId = "by.godevelopment.designpatterns"
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val jvmTarget = "1.8"
+}
